96f088a2f939ac799ab8f5c23d719906c9b533ba
Christian <oterogchristian17@gmail.com>
2024-03-19 18:46:39 -0600

    Segunda Actualizacion de Proyecto
    - En proceso Web Scrapping

--- ML/Producto.cs
+++ ML/Producto.cs
@@ -11,5 +11,7 @@ namespace ML
         public string? Nombre { get; set; }
         public string? Descripcion { get; set; }
         public string? Costo { get; set; }
+
+        public List<Producto> Productos { get;}
     }
 }
--- PL/Controllers/WebScrappingController.cs
+++ PL/Controllers/WebScrappingController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using static System.Collections.Specialized.BitVector32;
+using DL;
 
 namespace PL.Controllers
 {
@@ -17,42 +19,77 @@ namespace PL.Controllers
             IWebElement resultsPanel = driver.FindElement(By.ClassName("nav-icon-search"));
             resultsPanel.Click();
 
-          
 
-            IWebElement nameProduct = driver.FindElement(By.ClassName("ui-search-item__brand-discoverability ui-search-item__group__element"));
-
-            Console.WriteLine($"Name: {nameProduct}");
-
-            //var products = new List<Product>();
-
-            //var nameElement = driver.FindElement(By.XPath("//div[@class='ui - search - item__title']"));
-
-
-            //var name = nameElement.Text;
-
-            //var product = new Product { Nombre = name };
-
-            //products.Add(product);
+            List<string> misProductos = new List<string>();  
+            
+            foreach(var Nodo in driver.Url.Cast)
 
 
 
-            //foreach (var productElement in productElements)
-            //{
-            //    // select the name and price elements
-            //    var nameElement = productElement.FindElement(By.ClassName("ui - search - item__title"));
-            //    var priceElement = productElement.FindElement(By.ClassName("andes-money-amount__fraction"));
+            //   string productos = driver.FindElement(By.ClassName("ui-search"));
 
-            //    // extract their data
-            //    var name = nameElement.Text;
-            //    var price = priceElement.Text;
+            //       pokemons = driver.find_elements(By.XPATH, "//*[@id='main']/ul/li")
 
-            //    // create a new product object and add it to the list
-            //    var product = new Product { Nombre = name, Costo = price };
+            //       //*[@id="root-app"]
+  
 
-            //    products.Add(product);
-            //}
+            
+                var productos = driver.FindElements(By.XPath("//h2[@class='ui-search-item__title']"));
 
-            return View();
+                Console.WriteLine(productos);
+            
+
+      
+                //   }
+
+                //    for pokemon in pokemons:
+
+                //name = pokemon.find_element(By.XPATH, ".//h2").text
+
+                //price = pokemon.find_element(By.XPATH, ".//span").text
+
+                //img = pokemon.find_element(By.XPATH, ".//img").get_attribute("src")
+
+                //link = pokemon.find_element(By.XPATH, ".//a").get_attribute("href")
+
+                //*[@id=":Rgl9bb:"]
+                //*[@id="main"]/ul/li[3]
+                // Console.WriteLine(precio);
+
+                //IWebElement nameProduct = driver.FindElement(By.ClassName("ui-search-item__brand-discoverability ui-search-item__group__element"));
+
+                //Console.WriteLine($"Name: {nameProduct}");
+
+                //var products = new List<Product>();
+
+                //var nameElement = driver.FindElement(By.XPath("//div[@class='ui - search - item__title']"));
+
+
+                //var name = nameElement.Text;
+
+                //var product = new Product { Nombre = name };
+
+                //products.Add(product);
+
+
+
+                //foreach (var productElement in productElements)
+                //{
+                //    // select the name and price elements
+                //    var nameElement = productElement.FindElement(By.ClassName("ui - search - item__title"));
+                //    var priceElement = productElement.FindElement(By.ClassName("andes-money-amount__fraction"));
+
+                //    // extract their data
+                //    var name = nameElement.Text;
+                //    var price = priceElement.Text;
+
+                //    // create a new product object and add it to the list
+                //    var product = new Product { Nombre = name, Costo = price };
+
+                //    products.Add(product);
+                //}
+
+                return View();
 
         }
     }
